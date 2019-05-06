bb9c529264816221a6517f613e5fdeb006992291
Gracias claude <31932887+graciasc@users.noreply.github.com>
2019-05-06 17:08:58 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,26 @@ export default class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      pokemon: []
+      pokemon: [],
+      Species: []
     };
   }
   async componentDidMount() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon");
     const json = await response.json();
     this.setState({ pokemon: json });
+    const pokemonName = json.results.map(name => {
+      return name.name;
+    });
+    console.log(pokemonName);
   }
   render() {
-    const { pokemon } = this.state;
-    console.log(pokemon.id);
+    // const { pokemon } = this.state;
+
+    // const name = pokemon.results.map(name => {
+    //   return name
+    // })
+    // console.log(name)
 
     return (
       <div>
