98766a95b184c927647c189a6b8edbef2aec1203
Gracias claude <31932887+graciasc@users.noreply.github.com>
2019-05-09 18:14:03 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,30 +7,67 @@ export default class App extends React.Component {
     super();
     this.state = {
       pokemon: [],
-      Species: []
+      Species: [],
+      search: "",
+      isLoading: false
     };
+    this.searchInput = this.searchInput.bind(this);
+  }
+
+  searchInput(e) {
+    this.setState({
+      search: e.target.value
+    });
   }
   async componentDidMount() {
+    // this.state.isLoading = true have to set isLoading to true to Load the page
+    this.setState({ isLoading: true });
     const response = await fetch("https://pokeapi.co/api/v2/pokemon");
     const json = await response.json();
-    this.setState({ pokemon: json });
-    const pokemonName = json.results.map(name => {
-      return name.name;
+    this.setState({
+      pokemon: json.results,
+      isLoading: false
     });
-    console.log(pokemonName);
+    // console.log(this.state.pokemon);
+    // const pokemonName = json.results.map(name => {
+    //   return name.name;
+    // });
+    // console.log(pokemonName);
   }
   render() {
-    // const { pokemon } = this.state;
+    console.log(this.state.isLoading); // working loading
+    // console.log(this.state.pokemon); pokemon names
 
-    // const name = pokemon.results.map(name => {
-    //   return name
-    // })
-    // console.log(name)
+    const filterNames = this.state.pokemon.filter(search => {
+      return (
+        search.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !==
+        -1
+      );
+    });
+    //search filter through pokemon Api
+    if (this.state.isLoading) {
+      return <p>is Loading..</p>;
+    }
 
     return (
       <div>
         <Header />
-        <p> Pokemon</p>
+        <form>
+          <input
+            type="text"
+            placeholder="Pokemon"
+            onChange={this.searchInput}
+          />
+        </form>
+        {filterNames.map(pokemon => {
+          return <li> {pokemon.name}</li>;
+        })}
+        {/* <h1> {this.state.search}</h1>  the updating state*/}
+        {/* <h1> {this.state.pokemon[1]}</h1> */}
+
+        {/* {pokemonName.map((pokemon)=> {
+  return  <p>{pokemon} </p>
+})} */}
       </div>
     );
   }
