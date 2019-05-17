45b179eb36af3714766e1401a7e4a9e0ac9ebedd
Graciasc <gracias.claude@hotmail.com>
2019-05-16 23:26:51 -0600

    retrieved the data from the api pokeapi.co

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import Route from './routes/Routes';
 import { BrowserRouter } from 'react-router-dom';
-import { Navbar,Footer} from './components';
+import { Navbar, Footer } from './components';
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Route />
-        <Footer/>
-      </BrowserRouter>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      
+    };
+  }
+  async componentDidMount() {
+const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+const data = await response.json()
+console.log(data) // recieves the api data
+
+  }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          {/* maybe a component that renders the api with pokemon=this.state.pokemon*/}
+          <Route />
+          <Footer />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
