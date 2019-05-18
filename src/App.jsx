7c07af2ddec3844afaf7c78c93695f43d606a1d6
Graciasc <gracias.claude@hotmail.com>
2019-05-18 15:13:34 -0600

    added state isLoading

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,40 @@ export default class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
-        pokemonList:{},
-        pokemonSpecialName:{}
+      pokemonList: {},
+      pokemonSpecialName: {},
+      isLoading:false
     };
   }
   async componentDidMount() {
-      try {
-
-          const response = await fetch('https://pokeapi.co/api/v2/pokemon')
-          const data = await response.json()
-          console.log(data) // recieves the api data
-          const urls = data.results.map(name=> name.url)
-          console.log(urls)
-          this.setState=({pokemonList:data.results,
-        pokemonUrl:await fetch(urls)})
-
-
-        }
-        catch(error) {
-            if(error) {
-                return `The error you received is ${error}`
-            }
-        }
+    try {
+      this.setState({isLoading:true})
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const data = await response.json();
+      console.log(data); // recieves the api data
+      const urls = data.results.map(name => name.url);
+      console.log(urls);
+      this.setState = {
+        pokemonList: data.results,
+        pokemonUrl: await fetch(urls),
+        isLoading:false
+      };
+    } catch (error) {
+      if (error) {
+        return `The error you received is ${error}`;
+      }
     }
+  }
   render() {
+    const loadString = 'Loading...'
+    // const name = this.state.isLoading === true ? this.state.pokemonList.map(item=> <li>{item} </li>) : loadString
+
     return (
       <div>
         <BrowserRouter>
           <Navbar />
           {/* maybe a component that renders the api with pokemon=this.state.pokemon*/}
+          {name}
           <Route />
           <Footer />
         </BrowserRouter>
