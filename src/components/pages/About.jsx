499c6b5b5a358e5282a37a39c1c5425d9a6d6af9
Graciasc <gracias.claude@hotmail.com>
2019-06-01 08:20:39 -0600

    imported pokemon API and a Form

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,6 +4,8 @@ export default function About() {
   return (
     <div style={{minHeight: "99vh"}}>
       <h1> About</h1>
+      <p> I am a Computer science who love to program. I decided to build this little Website/App to get my hands wet with an API.
+    I used  </p>
     </div>
   );
 }
--- src/components/pages/Pokemon.jsx
+++ src/components/pages/Pokemon.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 
 
-export default function Pokemon () {
+
+export default class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: [],
+      value: ""
+    };
+    this.search = this.search.bind(this);
+  }
+
+  search() {
+    // this will search the input in the this.state.data
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = await response.json();
+    this.setState({ loading: false, data: data.results });
+    console.log(this.state.data);
+  }
+
+  render() {
+    const name = this.state.data.map((poke, i) => (
+      <li key={i}> {poke.name}</li>
+    ));
+    const loading = this.state.loading ? "...Loading" : name;
     return (
-        <div style={{minHeight: "99vh"}}>
-<h1> Pokemon</h1>
-        </div>
-    )
+      <div className="App"style={{minHeight: "99vh"}} >
+        <h1>Pokemon Names </h1>
+        <form>
+          <input
+            value=""
+            placeholder="Search List for Pokemon"
+            onChange={this.state.search}
+          />
+          {loading}
+        </form>
+      </div>
+    );
+  }
 }
