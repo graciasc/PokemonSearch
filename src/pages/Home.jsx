a7199bdccf9e7b8d90ac67a9aeb5c3b0231d6313
Graciasc <gracias.claude@hotmail.com>
2019-06-14 10:58:46 -0600

    added a picture on the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import img from '../assets/graciasc.jpg'
+import {Segment,Image} from 'semantic-ui-react'
 
 export default function Home() {
+
   return (
     <div style={{minHeight: "99vh"}}>
-      <h1> Home</h1>
+      {/* <h1> Home</h1> */}
+      <Segment attached padded='very'  >
+      <Image circular centered bordered fluid size='big' src={img}/>
+         {/* <Segment attached> */}
+
+      {/* </Segment> */}
+      </Segment>
+
     </div>
   );
 }
