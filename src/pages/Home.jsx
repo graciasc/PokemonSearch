4a07d3ff7ff722efcd7c06bf6a4264a27f60b57f
Graciasc <gracias.claude@hotmail.com>
2019-06-14 11:26:38 -0600

    removed About page from routing added info on the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import img from '../assets/graciasc.jpg'
-import {Segment,Image} from 'semantic-ui-react'
+import {Segment,Image,Grid, Header} from 'semantic-ui-react'
 
 export default function Home() {
 
   return (
     <div style={{minHeight: "99vh"}}>
-      {/* <h1> Home</h1> */}
       <Segment attached padded='very'  >
       <Image circular centered bordered fluid size='big' src={img}/>
-         {/* <Segment attached> */}
-
-      {/* </Segment> */}
+      <Grid centered padded columns={4}>
+    <Grid.Column >
+<Header as='h1'content='Welcome'/>
+<p > I am a Computer science who loves to program. I decided to build this little Website/App to get my hands dirty with an API and to get more experience with building a full project.
+  </p>
+    </Grid.Column>
+</Grid>
       </Segment>
 
     </div>
